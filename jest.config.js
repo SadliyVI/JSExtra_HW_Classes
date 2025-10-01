d7fcbf02a3122e67231d143ae8092ffd99bbd8b9
V.Sadliy <sadliy@mail.ru>
2025-10-01 19:11:01 +0300

    Webpack platform changed

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 export default {
   testEnvironment: 'node',
   roots: ['<rootDir>/__tests__'],
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
   moduleFileExtensions: ['js'],
   collectCoverage: true,
   collectCoverageFrom: ['src/classes/**/*.js'],
